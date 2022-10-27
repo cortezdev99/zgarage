303ded037b7eccca99337d2a790359c3c640155c
Michael Cortez <CortezDevelopement@gmail.com>
2022-10-27 15:39:56 -0600

    finished basic layout of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,28 +5,58 @@ const Navbar = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <div>
-        <img src="https://via.placeholder.com/200x225"></img>
+        <img style={{ display: "flex" }} src="https://via.placeholder.com/150x175"></img>
       </div>
 
       <div style={{ paddingTop: "20px", textAlign: "center", margin: "20px 5%", width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-evenly" }}>
-        <div style={{ width: "100px" }}>
-          Home <FontAwesomeIcon icon={['fas', 'trash-alt']} />
+        <div style={{ width: "100px", cursor: "pointer" }}>
+          <div style={{ fontSize: "20px" }}>
+            <FontAwesomeIcon icon={['fas', 'home']} />
+          </div>
+
+          <div style={{ paddingTop: "10px", fontSize: "14px" }}>
+            Home 
+          </div>
         </div>
 
-        <div style={{ width: "100px" }}>
-          Book us
+        <div style={{ width: "100px", cursor: "pointer" }}>
+          <div style={{ fontSize: "20px" }}>
+            <FontAwesomeIcon icon={['far', 'calendar-check']} />
+          </div>
+
+          <div style={{ paddingTop: "10px", fontSize: "14px" }}>
+            Book us 
+          </div>
         </div>
 
-        <div style={{ width: "100px" }}>
-          Pricing
+        <div style={{ width: "100px", cursor: "pointer" }}>
+          <div style={{ fontSize: "20px" }}>
+            <FontAwesomeIcon icon={['fas', 'tag']} />
+          </div>
+
+          <div style={{ paddingTop: "10px", fontSize: "14px" }}>
+            Pricing 
+          </div>
         </div>
 
-        <div style={{ width: "100px" }}>
-          Our team
+        <div style={{ width: "100px", cursor: "pointer" }}>
+          <div style={{ fontSize: "20px" }}>
+            <FontAwesomeIcon icon={['fas', 'id-card']} />
+          </div>
+
+          <div style={{ paddingTop: "10px", fontSize: "14px" }}>
+            Our team 
+          </div>
         </div>
 
-        <div style={{ width: "100px" }}>
-          Photo gallery
+        <div style={{ width: "100px", cursor: "pointer" }}>
+          <div style={{ fontSize: "20px" }}>
+            <FontAwesomeIcon icon={['fas', 'images']} />
+          </div>
+
+          <div style={{ paddingTop: "10px", fontSize: "14px" }}>
+            Photo gallery 
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Utilities/Icons.jsx
+++ src/Components/Utilities/Icons.jsx
@@ -1,49 +1,31 @@
 // fas icons
 import {
-  faBars,
-  faChevronUp,
-  faChevronDown,
-  faChevronRight,
-  faChevronLeft,
-  faPlus,
-  faTrashAlt,
-  faSearchPlus,
-  faPhoneVolume,
-  faExclamation
+  faHome,
+  faTag,
+  faIdCard,
+  faImages
 } from "@fortawesome/free-solid-svg-icons";
 
 // far icons
 import {
-    faCopyright,
-    faSquare,
-    faCheckSquare,
-    faClock
-  } from "@fortawesome/free-regular-svg-icons";
+  faCalendarCheck
+} from "@fortawesome/free-regular-svg-icons";
 
 // fab icons
-import { faInstagram, faFacebookF, faTwitter } from '@fortawesome/free-brands-svg-icons' 
+import { 
+  
+
+} from '@fortawesome/free-brands-svg-icons' 
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
   return library.add(
-    faBars,
-    faInstagram,
-    faFacebookF,
-    faTwitter,
-    faCopyright,
-    faChevronUp,
-    faChevronDown,
-    faChevronLeft,
-    faSquare,
-    faCheckSquare,
-    faChevronRight,
-    faPlus,
-    faTrashAlt,
-    faSearchPlus,
-    faPhoneVolume,
-    faClock,
-    faExclamation
+     faHome,
+     faCalendarCheck,
+     faTag,
+     faIdCard,
+     faImages
   );
 };
 
