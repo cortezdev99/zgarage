17db783cc025ae31a41b7a2ac48fbb74e8669277
Michael Cortez <CortezDevelopement@gmail.com>
2022-10-27 13:33:11 -0600

    initial commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,12 @@
 import "../Styles/index.scss";
+import Navbar from "./Navbar/Navbar";
 
 const App = () => {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
