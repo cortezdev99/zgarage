a27c6c64522b2d9b174b0dab698f32db7cb0d585
Michael Cortez <CortezDevelopement@gmail.com>
2022-11-01 14:10:57 -0600

    finished homepage layout and positioning until I swap template images for live images, as well as color scheme.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,29 +8,55 @@ const Footer = () => {
         <img src="https://via.placeholder.com/150x150" />
       </div>
 
-      <div style={{ width: "100%", display: "flex", justifyContent: "space-evenly" }}>
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", margin: "0px 20%", width: "60%" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ paddingLeft: "10px", fontSize: "20px", transform: "rotate(135deg)" }}>
+            <FontAwesomeIcon icon={['fas', 'phone']} />
+          </div>
+
+          <div style={{ fontSize: "15px", cursor: "pointer" }}>
+            <div>(801) 259-4056</div>
+            <div style={{ fontSize: "14px" }}>9:00 AM - 5:00 PM</div>
+          </div>
+        </div>
+
+        <div style={{ textAlign: "end", display: "flex", flexDirection: "row-reverse", alignItems: "center" }}>
+          <div style={{ paddingLeft: "10px", fontSize: "20px" }}>
+            <FontAwesomeIcon icon={['fas', 'map-pin']} />
+          </div>
+
+          <div style={{ fontSize: "15px", cursor: "pointer" }}>
+            2423 S 500 W Bountiful,
+            <div>UT, 84010</div>
+          </div>
+          
+        </div>
+      </div>
+
+      {/* <div style={{ width: "100%", display: "flex", justifyContent: "space-evenly" }}>
         <div>(801) 259-4056</div>
         <div>10:00 AM - 5:00 PM</div>
+      </div> */}
+
+      <div style={{ fontSize: "14px", letterSpacing: "0.75px", lineHeight: "25px", width: "100%", display: "flex", justifyContent: "space-evenly", textAlign: "center" }}>
+        <div style={{ width: "120px", cursor: "pointer" }}>Home</div>
+        <div style={{ width: "120px", cursor: "pointer"  }}>Book Us</div>
+        <div style={{ width: "120px", cursor: "pointer"  }}>Pricing</div>
+        <div style={{ width: "120px", cursor: "pointer"  }}>Our Team</div>
+        <div style={{ width: "120px", cursor: "pointer"  }}>Photo Gallery</div>
       </div>
 
-      <div style={{ width: "100%", display: "flex", justifyContent: "space-evenly", textAlign: "center" }}>
-        <div style={{ width: "120px" }}>Home</div>
-        <div style={{ width: "120px" }}>Book Us</div>
-        <div style={{ width: "120px" }}>Pricing</div>
-        <div style={{ width: "120px" }}>Our Team</div>
-        <div style={{ width: "120px" }}>Photo Gallery</div>
-      </div>
-
-      <div style={{ width: "100%", display: "flex", justifyContent: "space-evenly" }}>
-        <div>
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <div style={{ fontSize: "24px", marginRight: "40px", cursor: "pointer" }}>
           <FontAwesomeIcon icon={['fab', 'facebook-square']} />
         </div>
-        <div>
+
+        <div style={{ fontSize: "24px", cursor: "pointer" }}>
           <FontAwesomeIcon icon={['fab', 'instagram']} />
         </div>
       </div>
 
-      <div>
+      <div style={{ fontSize: "12px" }}>
         Copyright @2022 All Rights Reserved.
       </div>
     </div>
