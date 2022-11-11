41b29f1d91e97c2271e6fe094ea4917d0d9fd2c7
Michael Cortez <CortezDevelopement@gmail.com>
2022-11-11 16:40:21 -0700

    added basic grid layout for image gallery component

--- src/Components/PhotoGallery/PhotoGallery.jsx
+++ src/Components/PhotoGallery/PhotoGallery.jsx
@@ -3,7 +3,48 @@ import React from 'react'
 const PhotoGallery = () => {
   return (
     <div>
-      Gallery
+      <div style={{ position: "relative", width: "100%", display: "flex", overflow: "hidden" }}>
+        <img 
+          style={{ position: "relative", top: "-40px", width: "100%", objectPosition: "center", transform: "skewY(-5deg)", transformOrigin: "top left" }}
+          src="https://via.placeholder.com/400x200"
+        />
+      </div>
+
+      <div style={{ padding: "40px" }}>
+        <div className="gallery-grid-container">
+          <div className="h-stretch">
+            <img src="https://source.unsplash.com/random/300x150" />
+          </div>
+
+          <div className="v-stretch">
+            <img src="https://source.unsplash.com/random/150x250" />
+          </div>
+
+          <div>
+            <img src="https://source.unsplash.com/random/150x150" />
+          </div>
+
+          <div className="v-stretch">
+            <img src="https://source.unsplash.com/random/150x250" />
+          </div>
+
+          <div className="v-stretch">
+            <img src="https://source.unsplash.com/random/150x250" />
+          </div>
+
+          <div>
+            <img src="https://source.unsplash.com/random/150x150" />
+          </div>
+
+          <div>
+            <img src="https://source.unsplash.com/random/150x150" />
+          </div>
+
+          <div className="h-stretch">
+            <img src="https://source.unsplash.com/random/300x150" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
