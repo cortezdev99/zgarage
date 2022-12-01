6bed71e157c07f5ced514a570e7effab0ea8fbb4
Michael Cortez <CortezDevelopement@gmail.com>
2022-11-30 19:33:12 -0700

    created a function in PricingModal that handles clearing data and closing it

--- src/Components/Pricing/PricingModal.jsx
+++ src/Components/Pricing/PricingModal.jsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState, useEffect } from 'react'
 import { useContext } from 'react';
 import PricingContext from '../../Contexts/PricingContext';
@@ -11,28 +12,14 @@ const PricingModal = () => {
 
   const {
     isModalOpen,
+    setIsModalOpen,
     isEditing,
-    docToUpdate
+    setIsEditing,
+    docToUpdate,
+    setDocToUpdate
   } = useContext(PricingContext);
 
   useEffect(() => {
-    // if (isEditing && !loading) {
-    //   return setLoading(true);
-    // } else if (isEditing && loading) {
-      // const {
-      //   breed,
-      //   bathAndBrushPrice,
-      //   fullServiceGroomPrice
-      // } = docToUpdate.data();
-
-      // setBreed(breed);
-      // setBathAndBrushPrice(bathAndBrushPrice);
-      // setFullServiceGroomPrice(fullServiceGroomPrice);
-      // return setLoading(false);
-    // } else {
-    //   return;
-    // }
-
     if (isEditing) {
       setLoading(true);
 
@@ -54,10 +41,24 @@ const PricingModal = () => {
     return;
   }, [ isEditing ])
 
+  const handleClosingModal = () => {
+    if (isEditing) {
+      setDocToUpdate(null);
+      setIsEditing(false);
+      setIsModalOpen(false);
+
+      setTimeout(() => {
+        const bodyElem = document.getElementById("body");
+        
+        return bodyElem.classList.remove("modal-open-disable-body-scroll");
+      }, 500)
+    }
+  }
+
   if (isModalOpen) {
     return (
       <div style={{ position: "fixed", height: "100vh", width: "100vw", zIndex: "100", background: "rgb(0 0 0 / 32%)", top: "0", left: "0", display: "flex", alignItems: "center", justifyContent: "center" }}>
-        <div style={{ width: "80%", height: "70%", backgroundColor: "#FFF", borderRadius: "10px" }}>
+        <div style={{ width: "80%", height: "70%", backgroundColor: "#F1F1F1", borderRadius: "10px" }}>
           {
             loading ? (
               <div style={{ height: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly" }}>
@@ -71,12 +72,17 @@ const PricingModal = () => {
               </div>
             ) : (
               <div style={{ overflowY: "auto" }}>
-                <div style={{ fontSize: "18px", padding: "20px", textAlign: "center", color: "#264a73", letterSpacing: "0.75px", fontWeight: "600", display: "flex", alignItems: "center", justifyContent: "center", lineHeight: "25px", position: "relative" }}>
+                <div style={{ fontSize: "18px", padding: "20px", textAlign: "center", color: "#264a73", letterSpacing: "0.75px", fontWeight: "600", display: "flex", alignItems: "center", justifyContent: "center", lineHeight: "25px", position: "relative", borderBottom: "2px solid #264a73", backgroundColor: "#FFF", borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}>
                   {
                     isEditing ? "Edit Service" : "New Service"
                   }
 
-                  <div style={{ fontSize: "20px", position: "absolute", top: "18px", right: "20px" }}>x</div>
+                  <div 
+                    style={{ fontSize: "20px", position: "absolute", top: "18px", right: "20px", cursor: "pointer" }}
+                    onClick={handleClosingModal}
+                  >
+                    <FontAwesomeIcon icon={['fas', 'xmark']} />
+                  </div>
                 </div>
               </div>
             )
