3cc1c191cd358984c84379a50f258916670aa52d
Michael Cortez <CortezDevelopement@gmail.com>
2022-12-01 18:32:10 -0700

    added comments to pricingModal to help myself remember what im doing

--- src/Components/Pricing/PricingModal.jsx
+++ src/Components/Pricing/PricingModal.jsx
@@ -11,7 +11,8 @@ const PricingModal = () => {
   const [ bathAndBrush, setBathAndBrush ] = useState("");
   const [ fullServiceGroom, setFullServiceGroom ] = useState("");
   const [ loadingSubmission, setLoadingSubmission ] = useState(false);
-
+  const [ error, setError ] = useState(false);
+  
   const {
     isModalOpen,
     setIsModalOpen,
@@ -24,33 +25,44 @@ const PricingModal = () => {
   } = useContext(PricingContext);
 
   useEffect(() => {
+    // if admin is editing a document, and the document has been passed to pricingModal provider, we need get the document data, and update state respectively
     if (isEditing && docToUpdate) {
+      // deconstructing document recieved from pricing modal provider
       const {
         breed,
         bathAndBrush,
         fullServiceGroom
       } = docToUpdate.data();
       
+      // updating state respectively with deconstructed document data
       setBreed(breed);
       setBathAndBrush(bathAndBrush);
       setFullServiceGroom(fullServiceGroom);
       setLoading(false);
     }
+    // declaring docToUpdate as a dependancy for useEffect hook
   }, [docToUpdate])
 
   useEffect(() => {
+    // if admin is not editing a document and the modal is open, set loading to false as there is no data being fetched.  
     if (!isEditing && isModalOpen) {
+      // admin is not editing document
       return setLoading(false);
     }
+    // declaring isEditing as a dependancy for useEffect hook
   }, [ isEditing ])
 
   useEffect(() => {
+    // if loading submission state has updated, continue with form submission -- this is indicate to user this process may take time
     if (loadingSubmission) {
+      // loading submission state has been updated, form submission begins
       handleSubmittingForm();
     }
+    // declaring loadingSubmission as a dependency for useEffect hook
   }, [loadingSubmission])
 
   const handleClosingModal = () => {
+    // reset modal states, and provider states
     setBreed("");
     setBathAndBrush("");
     setFullServiceGroom("");
@@ -59,39 +71,57 @@ const PricingModal = () => {
     setIsEditing(false);
     setIsModalOpen(false);
 
+    // wait 500ms providing some time for modal states to clear
     setTimeout(() => {
+      // creating ref to the body element
       const bodyElem = document.getElementById("body");
       
+      // removing class from body that disables scroll
       return bodyElem.classList.remove("modal-open-disable-body-scroll");
     }, 500)
   }
 
   const handleSubmittingForm = () => {
+    // clear existing errors on submission
+    setError("");
+
+    // prepare data being added to database
     const data = {
       breed,
       bathAndBrush,
       fullServiceGroom
     }
 
+    // make sure the form is filled out properly before submission
+    if (breed.length === 0 || bathAndBrush.length === 0 || fullServiceGroom.length === 0) {
+      // if not return user frienldy error message 
+      return setError("One of the required parameters is missing. Fill out the form correctly and submit again.");
+    }
+
+    // if form submission is an edit, use the updateDoc method, else use addDoc method
     if (isEditing) {
+      // creating ref to the document being updated
       const docRef = doc(db, 'pricing', docToUpdate.id)
 
+      // use updateDoc method
       updateDoc(docRef, data).then(() => {
+        // if successful close modal
         return handleClosingModal();
       }).catch((err) => {
-
-        // TODO ERROR HANDLING
-        console.log(err)
+        // otherwise return user friendly error message
+        return setError(err.message);
       })
     } else {
+      // created ref to the collection the document is being added to
       const collectionRef = collection(db, 'pricing');
   
+      // use addDoc method
       addDoc(collectionRef, data).then(() => {
+        // if successful close modal
         return handleClosingModal();
       }).catch((err) => {
-  
-        // TODO ERROR HANDLING
-        console.log(err)
+        // otherwise return user friendly error message
+        return setError(err);
       })
     }
   }
@@ -164,6 +194,14 @@ const PricingModal = () => {
                       style={{ height: "40px", borderRadius: "5px", border: "2px solid #264a73", padding: "0px 20px" }}
                     />
                   </div>
+
+                  {
+                    error ? (
+                      <div style={{ color: "#ff2424", fontSize: "14px", fontWeight: "600", textAlign: "center", padding: "0px 20px" }}>
+                        {error}
+                      </div>
+                    ) : null
+                  }
 
                   <button onClick={() => setLoadingSubmission(true)} style={{ cursor: "pointer", minWidth: "150px", padding: "10px 20px", lineHeight: "25px", height: "40px", width: "60%", backgroundColor: "#264a73", color: "#FFF", borderRadius: "7.5px", border: "none", fontWeight: "600", margin: "5px auto", letterSpacing: "0.75px" }}>
                     SUBMIT
