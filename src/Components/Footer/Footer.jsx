197221011cba8a1077e54357e8138ad061a47dcf
Michael Cortez <CortezDevelopement@gmail.com>
2023-02-20 23:34:30 -0700

    moved footer styles to its own dedicated styelsheet

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,50 +4,87 @@ import ZukiesGarageLogo from '../../Images/Logos/ZukiesGarage_logo.jpg'
 
 const Footer = () => {
   return (
-    <div style={{ backgroundColor: "#FFF", padding: "40px", display: "grid", gridTemplateColumns: "1fr", rowGap: "40px", justifyItems: "center" }}>
-      <div style={{ width: "100%", maxWidth: "150px" }}>
-        <img style={{ width: "100%", display: "flex" }} src={ZukiesGarageLogo} />
+    <div className="footer-wrapper">
+      <div className="footer-logo-wrapper">
+        <img 
+          className="footer-logo" 
+          src={ZukiesGarageLogo} 
+        />
       </div>
 
-      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", margin: "0px 20%", width: "60%" }}>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div style={{ paddingLeft: "10px", fontSize: "20px", transform: "rotate(135deg)" }}>
-            <FontAwesomeIcon icon={['fas', 'phone']} />
+      <div className="footer-important-info-wrapper">
+        <div className="footer-contact-info-container">
+          <div className="footer-contact-info-icon-wrapper">
+            <FontAwesomeIcon 
+              className="footer-contact-info-icon" 
+              icon={['fas', 'phone']}
+            />
           </div>
 
-          <div style={{ fontSize: "15px", cursor: "pointer" }}>
-            <div>(801) 259-4056</div>
-            <div style={{ fontSize: "14px" }}>9:00 AM - 5:00 PM</div>
+          <div className="footer-contact-info-wrapper">
+            <div className="footer-contact-info-phone-num">
+              (801) 259-4056
+            </div>
+
+            <div className="footer-contact-info-avail-hours">
+              9:00 AM - 5:00 PM
+            </div>
           </div>
         </div>
 
-        <div style={{ textAlign: "end", display: "flex", flexDirection: "row-reverse", alignItems: "center" }}>
-          <div style={{ paddingLeft: "10px", fontSize: "20px" }}>
-            <FontAwesomeIcon icon={['fas', 'map-pin']} />
+        <div className="footer-address-container">
+          <div className="footer-address-icon-wrapper">
+            <FontAwesomeIcon
+              className="footer-address-icon" 
+              icon={['fas', 'map-pin']}
+            />
           </div>
 
-          <div style={{ fontSize: "15px", cursor: "pointer" }}>
+          <div className="footer-address">
             2423 S 500 W Bountiful,
-            <div>UT, 84010</div>
+
+            <div className="footer-address-split">
+              UT, 84010
+            </div>
           </div>       
         </div>
       </div>
 
-      <div style={{ fontSize: "14px", letterSpacing: "0.75px", lineHeight: "25px", width: "100%", display: "flex", justifyContent: "space-evenly", textAlign: "center" }}>
-        <div style={{ width: "120px", cursor: "pointer" }}>Home</div>
-        <div style={{ width: "120px", cursor: "pointer"  }}>Book Us</div>
-        <div style={{ width: "120px", cursor: "pointer"  }}>Pricing</div>
-        <div style={{ width: "120px", cursor: "pointer"  }}>Our Team</div>
-        <div style={{ width: "120px", cursor: "pointer"  }}>Photo Gallery</div>
-      </div>
+      <div className="footer-nav-links-wrapper">
+        <div className="footer-nav-link">
+          Home
+        </div>
 
-      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-        <div style={{ fontSize: "24px", marginRight: "40px", cursor: "pointer" }}>
-          <FontAwesomeIcon icon={['fab', 'facebook-square']} />
+        <div className="footer-nav-link">
+          Book Us
+        </div>
+
+        <div className="footer-nav-link">
+          Pricing
+        </div>
+
+        <div className="footer-nav-link">
+          Our Team
+        </div>
+
+        <div className="footer-nav-link">
+          Photo Gallery
         </div>
       </div>
 
-      <div style={{ fontSize: "12px" }}>
+      <div className="footer-social-media-links-wrapper">
+        <div className="footer-social-media-link footer-social-media-link-full-text-wrapper">
+          <FontAwesomeIcon 
+            icon={['fab', 'facebook-square']}
+          /> 
+
+          <div className="footer-social-media-link-full-text">
+            Facebook
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-copyright-wrapper">
         Copyright @2022 All Rights Reserved.
       </div>
     </div>
