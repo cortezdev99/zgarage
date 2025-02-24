7c90f921b577ccba960743484b57bfce969f4ac7
Michael Cortez <CortezDevelopement@gmail.com>
2025-02-23 22:10:58 -0700

    new laptop pushing updates

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -66,228 +66,190 @@ const Pricing = () => {
   }, [pricingListData, search])
 
   return (
-    <div className="pricing-and-services-container">
-      <div className="pricing-and-services-wrapper">
-        <div className="services-heading-wrapper">
-          <div className="services-paw-print-group-wrapper">
-            <span className="services-paw-print-left">
-              <FontAwesomeIcon icon={['fas', 'paw']} />
-            </span>
+    <div className="pricing-container">
+      <div className="pricing-intro-wrapper">
+        <div className="pricing-intro-header">
+          Discover Our Competitive and Affordable Grooming Services!
+        </div>
+        
+        <div className="pricing-intro-text">
+          We understand the importance of maintaining your pet's appearance and hygiene, which is why we offer a range of competitively priced grooming services to meet your needs.
+        </div>
+      </div>
 
-            <span className="services-paw-print-straight">
-              <FontAwesomeIcon icon={['fas', 'paw']} />
-            </span>
-
-            <span className="services-paw-print-right">
-              <FontAwesomeIcon icon={['fas', 'paw']} />
-            </span>
+      <div className="services-wrapper">
+        <div className="service-wrapper">
+          <div className="service-header">
+            Bath and Brush
           </div>
 
-          <div className="services-heading">
-            Services
+          <div className="service-descript">
+            Treat your furry friend to the ultimate pampering experience with our comprehensive grooming service! Our skilled groomers provide a range of services, including high-quality shampooing, conditioning spray, fluff drying, brushing out, and even pad, face, and potty area trimming.
           </div>
 
-          <div className="services-paw-print-group-wrapper services-paw-print-right-group-wrapper">
-            <span className="services-paw-print-left">
-              <FontAwesomeIcon icon={['fas', 'paw']} />
-            </span>
+          <div className="service-price-range">
+            $25 - $45
+          </div>
 
-            <span className="services-paw-print-straight">
-              <FontAwesomeIcon icon={['fas', 'paw']} />
-            </span>
-
-            <span className="services-paw-print-right">
-              <FontAwesomeIcon icon={['fas', 'paw']} />
-            </span>
+          <div className="service-book-now-btn">
+            Book Now
           </div>
         </div>
 
-        <div className="services-wrapper">
-          <div className="services-group-container">
-            <div className="service-heading">Full Service Groom</div>
-
-            <ul className="services-group-wrapper">
-              <li className="service">High Quality Shampooing</li>
-              <li className="service">Conditioning Spray</li>
-              <li className="service">Fluff Dry</li>
-              <li className="service">Brush Out</li>
-              <li className="service">Nail Trim and Dremel</li>
-              <li className="service">Full Body Cut</li>
-              <li className="service">Gland Expression (Upon Request)</li>
-              <li className="service">Nail Trim and Dremel</li>
-              <li className="service">Ear Cleaning (Upon Request)</li>
-              <li className="service">Plently of Love and Pets!</li>
-            </ul>
+        <div className="service-wrapper service-wrapper-alt">
+          <div className="service-header">
+            Full Service Groom
           </div>
 
-          <div className="service-image-wrapper">
-            <img className="service-image" src={MouseTrap} />
-          </div>
-        </div>
-
-        <div className="services-wrapper">
-          <div className="service-image-wrapper service-image-wrapper-varient">
-            <img className="service-image" src="https://via.placeholder.com/250x250" />
+          <div className="service-descript">
+            Our expert groomers provide top-notch care and attention to your pet, starting with a high-quality shampooing and conditioning spray, followed by a gentle fluff dry and thorough brush out. We'll take care of your pet's nails with a trim and dremel, and give them a full body cut to keep them looking and feeling their best. Gland expression and ear cleaning are also available upon request.
           </div>
 
-          <div className="services-group-container">
-            <div className="service-heading">Bath and Brush</div>
-
-            <ul className="services-group-wrapper services-group-wrapper-varient">
-              <li className="service">High Quality Shampooing</li>
-              <li className="service">Conditioning Spray</li>
-              <li className="service">Fluff Dry</li>
-              <li className="service">Brush Out</li>
-              <li className="service">Nail Trim and Dremel</li>
-              <li className="service">Pad, Face, and Potty Area Trim</li>
-              <li className="service">Gland Expression (Upon Request)</li>
-              <li className="service">Nail Trim and Dremel</li>
-              <li className="service">Ear Cleaning (Upon Request)</li>
-              <li className="service">Lots of Cuddles</li>
-            </ul>
-          </div>
-        </div>
-
-        <div style={{ marginTop: "40px", textAlign: "center", paddingBottom: "10px", textTransform: "uppercase", letterSpacing: "0.75px", fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center" }}>
-          <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "5px", paddingRight: "10px" }}>
-            <span style={{ transform: "rotate(315deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
-            <span style={{ position: "relative", top: "-15px" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
-            <span style={{ transform: "rotate(45deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
+          <div className="service-price-range">
+            $45 - $76
           </div>
 
-          <div>
-            Pricing
-          </div>
-
-          <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "5px", paddingLeft: "10px" }}>
-            <span style={{ transform: "rotate(315deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
-            <span style={{ position: "relative", top: "-15px" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
-            <span style={{ transform: "rotate(45deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
-          </div>
-        </div>
-
-        <div style={{ width: "90%", border: "2.5px solid #e1e1e1", borderLeft: "1.5px solid #e1e1e1", borderRight: "1.5px solid #e1e1e1", borderRadius: "10px", backgroundColor: "#FFF", margin: "40px auto", boxShadow: "#e1e1e1 0px 12px 10px 0px" }}>
-          <div style={{ padding: "20px 5px", display: "grid", gridTemplateColumns: `${auth.currentUser ? "0.5fr 1fr 1fr 1fr" : "1fr 1fr 1fr" }`, textAlign: "center"}}>
-            { auth.currentUser ? (
-              <div onClick={() => handleOpeningModal("NEW_DOC")} style={{ fontSize: "18px", fontWeight: "600", color: "#264a73", cursor: "pointer" }}>
-                <FontAwesomeIcon icon={['fas', 'plus']} />
-              </div>
-            ) : null }
-            <div style={{ textDecorationLine: "underline" }}>Breed</div>
-            <div style={{ textDecorationLine: "underline" }}>Bath & Brush</div>
-            <div style={{ textDecorationLine: "underline" }}>Full Service Groom</div>
-          </div>
-
-          <div style={{ position: "relative" }}>
-            <input
-              style={{ 
-                width: "calc(100% - 50px)",
-                padding: "0px 20px",
-                height: "40px",
-                border: "none",
-                margin: "0px 5px",
-                backgroundColor: "#ececec70",
-                borderRadius: "2.5px"
-                }}
-              id="search-breed"
-              placeholder="Search by breed (Ex: doodle)"
-              type="text"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-
-            
-            {
-              search.length > 0 ? (
-                <div onClick={() => setSearch("")} style={{ cursor: "pointer", position: "absolute", top: "11px", right: "25px", fontSize: "17px" }}>
-                  <FontAwesomeIcon icon={['fas', 'xmark']} />
-                </div>
-              ) : (
-                <label for="search-breed" style={{ cursor: "pointer", position: "absolute", top: "11px", right: "25px", fontSize: "17px" }}>
-                  <FontAwesomeIcon icon={['fas', 'magnifying-glass']} />
-                </label>
-              )
-            }
-          </div>
-
-          <div>
-            {
-              pricingList && !loading && pricingList.length > 0 ? pricingList.map((price, idx) => {
-                const {
-                  breed,
-                  bathAndBrush,
-                  fullServiceGroom
-                } = price.data();
-              
-                return (
-                  <div key={idx} style={{ padding: "20px 5px", display: "grid", gridTemplateColumns: `${auth.currentUser ? "0.5fr 1fr 1fr 1fr" : "1fr 1fr 1fr" }`, textAlign: "center"}}>
-                    {
-                      auth.currentUser ? (
-                        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-evenly", fontSize: "18px" }}>
-                          <div style={{ color: "#ff2424" }}>
-                            <FontAwesomeIcon
-                              style={{ cursor: "pointer" }} 
-                              icon={['fas', 'trash-alt']}
-                              onClick={() => handleDeletingPrice(price.id)}
-                            />
-                          </div>
-
-                          <div style={{ color: "#264a73" }}>
-                            <FontAwesomeIcon
-                              style={{ cursor: "pointer" }} 
-                              icon={['fas', 'pen-square']}
-                              onClick={() => handleOpeningModal(price)}
-                            />
-                          </div>             
-                        </div>
-                      ) : null
-                    }
-                    <div>{breed}</div>
-                    <div>{bathAndBrush}</div>
-                    <div>{fullServiceGroom}</div>
-                  </div>
-                )
-              })
-              : loading ? (
-                <div style={{ padding: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-                  <div style={{ fontSize: "18px", paddingBottom: "10px" }}>
-                    Price List Loading
-                  </div>
-
-                  <div style={{ paddingBottom: "15px" }}>
-                    Loading content... Please wait...
-                  </div>
-
-                  <Loading small={true} />
-                </div>
-              ) : (
-                <div style={{ display: "flex" }}>
-                  <div style={{ width: "150px" }}>
-                    <img style={{ objectFit: "cover", width: "100%", height: "100%" }} src={wholeSomeDogImage} />
-                  </div>
-
-                  <div style={{ display: "flex", flex: "1", flexDirection: "column", justifyContent: "space-evenly", letterSpacing: "0.75px", padding: "0px 20px 0px 5px" }}>
-                    <div style={{ fontSize: "18px" }}>
-                      Uh-Oh
-                    </div>
-
-                    <div>
-                      It looks like we haven't added that breed yet. You can contact us <span style={{ textDecorationLine: "underline" }}>here</span>
-                    </div>
-                  </div>
-                </div>
-              )
-            }
+          <div className="service-book-now-btn service-book-now-btn-alt">
+            Book Now
           </div>
         </div>
       </div>
 
-      {
-        auth.currentUser ? (
-          <PricingModal />
-        ) : null
-      }
-    </div>
+      {/* SEPERATOR */}
+
+      <div style={{ marginTop: "40px", textAlign: "center", paddingBottom: "10px", textTransform: "uppercase", letterSpacing: "0.75px", fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "5px", paddingRight: "10px" }}>
+          <span style={{ transform: "rotate(315deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
+          <span style={{ position: "relative", top: "-15px" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
+          <span style={{ transform: "rotate(45deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
+        </div>
+
+        <div>
+          Pricing
+        </div>
+
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "5px", paddingLeft: "10px" }}>
+          <span style={{ transform: "rotate(315deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
+          <span style={{ position: "relative", top: "-15px" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
+          <span style={{ transform: "rotate(45deg)" }}><FontAwesomeIcon icon={['fas', 'paw']} /></span>
+        </div>
+      </div>
+
+      <div style={{ width: "90%", border: "2.5px solid #e1e1e1", borderLeft: "1.5px solid #e1e1e1", borderRight: "1.5px solid #e1e1e1", borderRadius: "10px", backgroundColor: "#FFF", margin: "40px auto", boxShadow: "#e1e1e1 0px 12px 10px 0px" }}>
+        <div style={{ padding: "20px 5px", display: "grid", gridTemplateColumns: `${auth.currentUser ? "0.5fr 1fr 1fr 1fr" : "1fr 1fr 1fr" }`, textAlign: "center"}}>
+          { auth.currentUser ? (
+            <div onClick={() => handleOpeningModal("NEW_DOC")} style={{ fontSize: "18px", fontWeight: "600", color: "#264a73", cursor: "pointer" }}>
+              <FontAwesomeIcon icon={['fas', 'plus']} />
+            </div>
+          ) : null }
+          <div style={{ textDecorationLine: "underline" }}>Breed</div>
+          <div style={{ textDecorationLine: "underline" }}>Bath & Brush</div>
+          <div style={{ textDecorationLine: "underline" }}>Full Service Groom</div>
+        </div>
+
+        <div style={{ position: "relative" }}>
+          <input
+            style={{ 
+              width: "calc(100% - 50px)",
+              padding: "0px 20px",
+              height: "40px",
+              border: "none",
+              margin: "0px 5px",
+              backgroundColor: "#ececec70",
+              borderRadius: "2.5px"
+              }}
+            id="search-breed"
+            placeholder="Search by breed (Ex: doodle)"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          
+          {
+            search.length > 0 ? (
+              <div onClick={() => setSearch("")} style={{ cursor: "pointer", position: "absolute", top: "11px", right: "25px", fontSize: "17px" }}>
+                <FontAwesomeIcon icon={['fas', 'xmark']} />
+              </div>
+            ) : (
+              <label for="search-breed" style={{ cursor: "pointer", position: "absolute", top: "11px", right: "25px", fontSize: "17px" }}>
+                <FontAwesomeIcon icon={['fas', 'magnifying-glass']} />
+              </label>
+            )
+          }
+        </div>
+
+        <div>
+          {
+            pricingList && !loading && pricingList.length > 0 ? pricingList.map((price, idx) => {
+              const {
+                breed,
+                bathAndBrush,
+                fullServiceGroom
+              } = price.data();
+            
+              return (
+                <div key={idx} style={{ padding: "20px 5px", display: "grid", gridTemplateColumns: `${auth.currentUser ? "0.5fr 1fr 1fr 1fr" : "1fr 1fr 1fr" }`, textAlign: "center"}}>
+                  {
+                    auth.currentUser ? (
+                      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-evenly", fontSize: "18px" }}>
+                        <div style={{ color: "#ff2424" }}>
+                          <FontAwesomeIcon
+                            style={{ cursor: "pointer" }} 
+                            icon={['fas', 'trash-alt']}
+                            onClick={() => handleDeletingPrice(price.id)}
+                          />
+                        </div>
+
+                        <div style={{ color: "#264a73" }}>
+                          <FontAwesomeIcon
+                            style={{ cursor: "pointer" }} 
+                            icon={['fas', 'pen-square']}
+                            onClick={() => handleOpeningModal(price)}
+                          />
+                        </div>             
+                      </div>
+                    ) : null
+                  }
+                  <div>{breed}</div>
+                  <div>{bathAndBrush}</div>
+                  <div>{fullServiceGroom}</div>
+                </div>
+              )
+            })
+            : loading ? (
+              <div style={{ padding: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <div style={{ fontSize: "18px", paddingBottom: "10px" }}>
+                  Price List Loading
+                </div>
+
+                <div style={{ paddingBottom: "15px" }}>
+                  Loading content... Please wait...
+                </div>
+
+                <Loading small={true} />
+              </div>
+            ) : (
+              <div style={{ display: "flex" }}>
+                <div style={{ width: "150px" }}>
+                  <img style={{ objectFit: "cover", width: "100%", height: "100%" }} src={wholeSomeDogImage} />
+                </div>
+
+                <div style={{ display: "flex", flex: "1", flexDirection: "column", justifyContent: "space-evenly", letterSpacing: "0.75px", padding: "0px 20px 0px 5px" }}>
+                  <div style={{ fontSize: "18px" }}>
+                    Uh-Oh
+                  </div>
+
+                  <div>
+                    It looks like we haven't added that breed yet. You can contact us <span style={{ textDecorationLine: "underline" }}>here</span>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </div>
+   </div>
   )
 }
 
